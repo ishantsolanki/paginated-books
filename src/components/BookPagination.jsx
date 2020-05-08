b5caf4f07c4a0e831eb41bdca93feaf9bbd8cb76
ishantsolanki <ishant360@gmail.com>
2020-05-08 10:09:06 +0100

    Add styles to main container and create Paginator

--- src/components/BookPagination.jsx
+++ src/components/BookPagination.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 import { loadBookApi } from '../redux/actions';
 
 import BooksList from '../components/BooksList';
+import Paginator from '../components/Paginator';
 
 const mapDispatchToProps = {
   loadBookApi
 };
+
+export const Header = ({ title }) => (
+  <>
+    <h2 className="d-flex justify-content-center">{title}</h2>
+    <hr/>
+  </>
+);
 
 export const BookPagination = ({
   loadBookApi,
@@ -20,10 +29,11 @@ export const BookPagination = ({
   }, [loadBookApi, page]);
 
   return (
-    <>
-      <div>Book Pagination - Page {page}</div>
+    <Jumbotron>
+      <Header title="Books from Greece"/>
       <BooksList />
-    </>
+      <Paginator />
+    </Jumbotron>
   );
 };
 
---
+++ src/components/Paginator.jsx
@@ -0,0 +1,3 @@
+export const Paginator = () => 'Paginator';
+
+export default Paginator;
