e1d006207522308e03067743c891df42279d9e6f
ishantsolanki <ishant360@gmail.com>
2020-05-10 21:34:03 +0100

    Remove unused css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import AppRouter from './router.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import './App.css';
 
 function App() {
   return (
